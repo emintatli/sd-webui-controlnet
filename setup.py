7519e97bf7143d7190492cca017c258a626648c4
Emin TATLI <emintatli06@gmail.com>
2023-07-30 03:08:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import launch
 import os
 import pkg_resources
 import sys
-
+from launch import run
 sys.append(os.getcwd())
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
@@ -14,9 +14,9 @@ with open(req_file) as file:
                 package_name, package_version = package.split('==')
                 installed_version = pkg_resources.get_distribution(package_name).version
                 if installed_version != package_version:
-                    launch.run_pip(f"install {package}", f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}")
+                    run(f"install {package}", f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}")
             elif not launch.is_installed(package):
-                launch.run_pip(f"install {package}", f"sd-webui-controlnet requirement: {package}")
+                run(f"install {package}", f"sd-webui-controlnet requirement: {package}")
         except Exception as e:
             print(e)
             print(f'Warning: Failed to install {package}, some preprocessors may not work.')
